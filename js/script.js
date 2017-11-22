8b32b8cf0e1b114d2f087736f9cb8ab9da3ec90a
danielld75 <danielld@poczta.fm>
2017-11-22 22:57:39 +0100

    Add first commit with triangle

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a = 1,
+  h = 1,
+  triangleArea = 1.0;
+alert("Obliczenia: Pole trójkąta.");
+a = prompt("Wprowadź liczbę dla a:");
+h = prompt("Wprowadź liczbę dla h:");
+triangleArea = a*h/2;
+alert("Pole trójkąta z danymi: a: " + a + " i wysokość h: " + h + " równa się: " + triangleArea);
