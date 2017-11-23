378c1d2906d032fd94febec85708942b9a6117e6
danielld75 <danielld@poczta.fm>
2017-11-23 06:05:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-var a = 1,
-  h = 1,
-  triangleArea = 1.0;
 alert("Obliczenia: Pole trójkąta.");
-a = prompt("Wprowadź liczbę dla a:");
-h = prompt("Wprowadź liczbę dla h:");
-triangleArea = a*h/2;
+var a = prompt("Wprowadź liczbę dla a:");
+var h = prompt("Wprowadź liczbę dla h:");
+var triangleArea = a * h / 2;
 alert("Pole trójkąta z danymi: a: " + a + " i wysokość h: " + h + " równa się: " + triangleArea);
